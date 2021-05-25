89a694db1a7193dba424ea86337433ad57c62d49
Anh <anh@canva.com>
2021-05-25 11:40:15 +1000

    add two divs to demonstrate

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import * as React from "react"
 import * as styles from './index.module.css'
 
-const IndexPage = () => <div className={styles.divTest}>CONTENT HERE</div>
+const IndexPage = () => <>
+  <div className={styles.divExternal}>div with color imported from another file</div>
+  <div className={styles.divInternal}>div with color from the same file</div>
+  </>
 
 
 export default IndexPage
